a0275412ada6e1a868a4de3e265d0fb4fa548cca
Mario Fernandez Adamez <mfernandezadamez@gmail.com>
2024-04-15 15:47:10 +0200

    COMMIT MARIO ALERTA_ADMIN

--- app/src/main/java/com/example/proyectotfg/ALERTA_ADMIN.java
+++ app/src/main/java/com/example/proyectotfg/ALERTA_ADMIN.java
@@ -1,8 +1,11 @@
 package com.example.proyectotfg;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ALERTA_ADMIN extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class ALERTA_ADMIN extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alerta_admin);
+
+        AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+        dialogo1.setTitle("Importante");
+        dialogo1.setMessage("¿ Acepta la ejecución de este programa en modo prueba ?");
+        dialogo1.setCancelable(false);
+        dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo1, int id) {
+                aceptar();
+            }
+        });
+        dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo1, int id) {
+                cancelar();
+            }
+        });
+        dialogo1.show();
+    }
+
+    public void aceptar() {
+        Toast t = Toast.makeText(this, "Bienvenido a probar el programa.", Toast.LENGTH_SHORT);
+        t.show();
+    }
+
+    public void cancelar() {
+        finish();
     }
 }
