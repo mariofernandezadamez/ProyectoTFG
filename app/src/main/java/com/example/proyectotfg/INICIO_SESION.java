1a74a5cc95322acaa4541a770c51c10cd6bb3d16
Mario Fernandez Adamez <mfernandezadamez@gmail.com>
2024-05-01 20:06:50 +0200

    COMMIT MARIO inicio de sesion, parking usuarios logica

--- app/src/main/java/com/example/proyectotfg/INICIO_SESION.java
+++ app/src/main/java/com/example/proyectotfg/INICIO_SESION.java
@@ -2,13 +2,22 @@ package com.example.proyectotfg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class INICIO_SESION extends AppCompatActivity {
-
+    Button btn_iniciar_sesion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
+        btn_iniciar_sesion = findViewById(R.id.ISBtnIniciarSesion);
     }
+    public void inicio(View view){
+        Intent i = new Intent(this, PARKING_ADMIN_P1.class);
+        startActivity(i);
+    }
+
 }
--- app/src/main/java/com/example/proyectotfg/PARKING_USUARIOS_P1.java
+++ app/src/main/java/com/example/proyectotfg/PARKING_USUARIOS_P1.java
@@ -2,13 +2,60 @@ package com.example.proyectotfg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PARKING_USUARIOS_P1 extends AppCompatActivity {
-
+    Button btn_perfil, btn_planta1, btn_planta2, btn_planta3, btn_disponible1, btn_disponible2, btn_disponible3, btn_disponible4, btn_disponible5, btn_disponible6, btn_disponible7, btn_disponible8, btn_disponible9,
+            btn_disponible10, btn_disponible11, btn_disponible12, btn_disponible13, btn_disponible14, btn_disponible15, btn_disponible16;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_usuarios_p1);
+        btn_perfil = findViewById(R.id.Usuarios_P1_Perfil);
+        btn_planta1 = findViewById(R.id.Usuarios_P1_p1);
+        btn_planta2 = findViewById(R.id.Usuarios_P1_p2);
+        btn_planta3 = findViewById(R.id.Usuarios_P1_p3);
+        btn_disponible1 = findViewById(R.id.usuariosBTNdispo1P1);
+        btn_disponible2 = findViewById(R.id.usuariosBTNdispo2P1);
+        btn_disponible3 = findViewById(R.id.usuariosBTNdispo3P1);
+        btn_disponible4 = findViewById(R.id.usuariosBTNdispo4P1);
+        btn_disponible5 = findViewById(R.id.usuariosBTNdispo5P1);
+        btn_disponible6 = findViewById(R.id.usuariosBTNdispo6P1);
+        btn_disponible7 = findViewById(R.id.usuariosBTNdispo7P1);
+        btn_disponible8 = findViewById(R.id.usuariosBTNdispo8P1);
+        btn_disponible9 = findViewById(R.id.usuariosBTNdispo9P1);
+        btn_disponible10 = findViewById(R.id.usuariosBTNdispo10P1);
+        btn_disponible11 = findViewById(R.id.usuariosBTNdispo11P1);
+        btn_disponible12 = findViewById(R.id.usuariosBTNdispo12P1);
+        btn_disponible13 = findViewById(R.id.usuariosBTNdispo13P1);
+        btn_disponible14 = findViewById(R.id.usuariosBTNdispo14P1);
+        btn_disponible15 = findViewById(R.id.usuariosBTNdispo15P1);
+        btn_disponible16 = findViewById(R.id.usuariosBTNdispo16P1);
     }
+    public void perfil (View view){
+        Intent i = new Intent(this, PERFIL_USUARIO.class);
+        startActivity(i);
+    }
+    public void planta1 (View view){
+        Intent i = new Intent(this, PARKING_USUARIOS_P1.class);
+        startActivity(i);
+    }
+    public void planta2 (View view){
+        Intent i = new Intent(this, PARKING_USUARIOS_P2.class);
+        startActivity(i);
+    }
+    public void planta3 (View view){
+        Intent i = new Intent(this, PARKING_USUARIOS_P3.class);
+        startActivity(i);
+    }
+    public void reservas (View view){
+        Intent i = new Intent(this, RESERVA_PLAZA.class);
+        startActivity(i);
+    }
+
 }
