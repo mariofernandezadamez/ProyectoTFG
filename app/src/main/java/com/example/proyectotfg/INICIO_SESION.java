e396ffd60621d4867f9fe7f9380d3e3ba8bf3c33
Mario Fernandez Adamez <mfernandezadamez@gmail.com>
2024-05-08 14:09:48 +0200

    COMMIT MARIO botones inicio de sesion y registro

--- app/src/main/java/com/example/proyectotfg/INICIO_SESION.java
+++ app/src/main/java/com/example/proyectotfg/INICIO_SESION.java
@@ -6,22 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class INICIO_SESION extends AppCompatActivity {
     Button btn_iniciar_sesion;
+    TextView btnregistro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
         btn_iniciar_sesion = findViewById(R.id.ISBtnIniciarSesion);
+        btnregistro = findViewById(R.id.ISTextRegistrate);
     }
     public void inicio(View view){
         Intent i = new Intent(this, PARKING_USUARIOS_P1.class);
         startActivity(i);
     }
+
     public void registro(View view){
         Intent i = new Intent(this, REGISTRO_USUARIO.class);
         startActivity(i);
     }
-
 }
--- app/src/main/java/com/example/proyectotfg/REGISTRO_USUARIO.java
+++ app/src/main/java/com/example/proyectotfg/REGISTRO_USUARIO.java
@@ -2,13 +2,30 @@ package com.example.proyectotfg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class REGISTRO_USUARIO extends AppCompatActivity {
-
+    Button btnatras, btnregistro;
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
+        btnregistro = findViewById(R.id.REBtnRegistrate);
+        btnatras = findViewById(R.id.btnatras);
+    }
+    public void atras(View view){
+        Intent i = new Intent(this, INICIO_SESION.class);
+        startActivity(i);
+    }
+    public void registrobtn(View view){
+        Intent i = new Intent(this, INICIO_SESION.class);
+        Toast.makeText(this, "Se ha registrado correctamente", Toast.LENGTH_SHORT).show();
+        startActivity(i);
     }
 }
