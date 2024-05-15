5c7645d0a137779a4d8ba0b296e16f2bb1e5f01f
Mario Fernandez Adamez <mfernandezadamez@gmail.com>
2024-05-15 19:37:25 +0200

    COMMIT MARIO botones

--- app/src/main/java/com/example/proyectotfg/ADMIN_HOME.java
+++ app/src/main/java/com/example/proyectotfg/ADMIN_HOME.java
@@ -6,23 +6,20 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class ADMIN_HOME extends AppCompatActivity {
-    ImageView imv;
+    ImageButton imv;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_admin_home);
-
         imv = findViewById(R.id.btnatras1);
-        imv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(ADMIN_HOME.this, INICIO_SESION.class);
-                startActivity(intent);
-            }
-        });
+    }
+    public void btnatrras(View view){
+        Intent i = new Intent(this, INICIO_SESION.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/proyectotfg/PERFIL_USUARIO.java
+++ app/src/main/java/com/example/proyectotfg/PERFIL_USUARIO.java
@@ -2,13 +2,22 @@ package com.example.proyectotfg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PERFIL_USUARIO extends AppCompatActivity {
+    ImageButton imgbtnatras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_usuario);
+        imgbtnatras = findViewById(R.id.btnatras3);
+    }
+    public void atrass (View view){
+        Intent i = new Intent(this, PARKING_USUARIOS_P1.class);
+        startActivity(i);
     }
 }
