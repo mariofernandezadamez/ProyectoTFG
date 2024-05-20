ac757ee26751b8505c686465b5c8cd2bb914d024
Emilio567 <emiliocalvo567@gmail.com>
2024-05-20 15:54:51 +0200

    COMMIT BBDD_USUARIOS

--- app/src/main/java/com/example/proyectotfg/INICIO_SESION.java
+++ app/src/main/java/com/example/proyectotfg/INICIO_SESION.java
@@ -8,6 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.Map;
 
 public class INICIO_SESION extends AppCompatActivity {
     Button btn_iniciar_sesion;
@@ -31,5 +39,27 @@ public class INICIO_SESION extends AppCompatActivity {
     public void aboutus(View view){
         Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://aparcafacil3.wordpress.com/"));
         startActivity(i);
+    }
+
+    private void ejecutarInicio_Sesion(){
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "http://192.168.91.1/bbdd_tfg/inicio_sesion.php", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (!response.isEmpty()){
+                    Intent intent = new Intent(getApplicationContext(),PARKING_USUARIOS_P1.class);
+                    startActivity(intent);
+                }
+                Toast.makeText(getApplicationContext(), "Usuario o contraseña incorrecto", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        })
+        {
+            protected Map
+        }
+
     }
 }
--- app/src/main/java/com/example/proyectotfg/REGISTRO_USUARIO.java
+++ app/src/main/java/com/example/proyectotfg/REGISTRO_USUARIO.java
@@ -64,6 +64,7 @@ public class REGISTRO_USUARIO extends AppCompatActivity {
 
 
     private void ejecutarServicio(){
+
         StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://192.168.91.1/bbdd_tfg/insertado_registro.php", new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
