c8af45ee4617f617fec9e73edb8de9ad5cf9693a
Mario Fernandez Adamez <mfernandezadamez@gmail.com>
2024-05-29 15:57:33 +0200

    COMMIT MARIO funcionalidad reservas en la bbdd planta 2 y 3 y cambio de nombre en botones

--- app/src/main/java/com/example/proyectotfg/PARKING_USUARIOS_P3.java
+++ app/src/main/java/com/example/proyectotfg/PARKING_USUARIOS_P3.java
@@ -2,18 +2,22 @@ package com.example.proyectotfg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class PARKING_USUARIOS_P3 extends AppCompatActivity {
     Button btn_perfil_p3;
     Button btn_planta1, btn_planta2, btn_planta3, btn_disponible1, btn_disponible2, btn_disponible3, btn_disponible4, btn_disponible5, btn_disponible6, btn_disponible7, btn_disponible8, btn_disponible9,
             btn_disponible10, btn_disponible11, btn_disponible12, btn_disponible13, btn_disponible14, btn_disponible15, btn_disponible16;
-
+    String num_documento = "";
+    String planta= "";
+    String plaza = "";
+    RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,15 +26,15 @@ public class PARKING_USUARIOS_P3 extends AppCompatActivity {
         btn_planta1 = findViewById(R.id.Usuarios_P3_p1);
         btn_planta2 = findViewById(R.id.Usuarios_P3_p2);
         btn_planta3 = findViewById(R.id.Usuarios_P3_p3);
-        btn_disponible1 = findViewById(R.id.usuariosBTNdispo1P3);
-        btn_disponible2 = findViewById(R.id.usuariosBTNdispo2P3);
-        btn_disponible3 = findViewById(R.id.usuariosBTNdispo3P3);
-        btn_disponible4 = findViewById(R.id.usuariosBTNdispo4P3);
-        btn_disponible5 = findViewById(R.id.usuariosBTNdispo5P3);
-        btn_disponible6 = findViewById(R.id.usuariosBTNdispo6P3);
-        btn_disponible7 = findViewById(R.id.usuariosBTNdispo7P3);
-        btn_disponible8 = findViewById(R.id.usuariosBTNdispo8P3);
-        btn_disponible9 = findViewById(R.id.usuariosBTNdispo9P3);
+        btn_disponible1 = findViewById(R.id.usuariosBTNdispo01P3);
+        btn_disponible2 = findViewById(R.id.usuariosBTNdispo02P3);
+        btn_disponible3 = findViewById(R.id.usuariosBTNdispo03P3);
+        btn_disponible4 = findViewById(R.id.usuariosBTNdispo04P3);
+        btn_disponible5 = findViewById(R.id.usuariosBTNdispo05P3);
+        btn_disponible6 = findViewById(R.id.usuariosBTNdispo06P3);
+        btn_disponible7 = findViewById(R.id.usuariosBTNdispo07P3);
+        btn_disponible8 = findViewById(R.id.usuariosBTNdispo08P3);
+        btn_disponible9 = findViewById(R.id.usuariosBTNdispo09P3);
         btn_disponible10 = findViewById(R.id.usuariosBTNdispo10P3);
         btn_disponible11 = findViewById(R.id.usuariosBTNdispo11P3);
         btn_disponible12 = findViewById(R.id.usuariosBTNdispo12P3);
@@ -38,6 +42,16 @@ public class PARKING_USUARIOS_P3 extends AppCompatActivity {
         btn_disponible14 = findViewById(R.id.usuariosBTNdispo14P3);
         btn_disponible15 = findViewById(R.id.usuariosBTNdispo15P3);
         btn_disponible16 = findViewById(R.id.usuariosBTNdispo16P3);
+
+
+        requestQueue = Volley.newRequestQueue(this);
+        System.out.println("Número de documento: " + num_documento);
+
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("num_documento")) {
+            num_documento = intent.getStringExtra("num_documento");
+        }
+        System.out.println("Número de documento: " + num_documento);
     }
     public void perfil3 (View view){
         Intent i = new Intent(this, PERFIL_USUARIO.class);
@@ -57,6 +71,21 @@ public class PARKING_USUARIOS_P3 extends AppCompatActivity {
     }
     public void reservas_3 (View view){
         Intent i = new Intent(this, RESERVA_PLAZA.class);
+        i.putExtra("num_documento", num_documento);
+        obtenerPlazaPlanta(view);
+        i.putExtra("plaza", plaza);
+        i.putExtra("numero_planta", planta);
         startActivity(i);
+    }
+
+    public void obtenerPlazaPlanta(View view) {
+        int id = view.getId();
+
+        String nombreRecurso = getResources().getResourceEntryName(id);
+
+        plaza = nombreRecurso.substring(nombreRecurso.length() - 4, nombreRecurso.length() - 2);
+        planta = nombreRecurso.substring(nombreRecurso.length() - 1);
+
+        System.out.println("Plaza: " + plaza + ", Planta: " + planta);
     }
 }
